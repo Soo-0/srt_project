3c706fa3f11e4c2e7c1a27f608a6d84d20084567
soo_0 <yoonsoo.0e@gmail.com>
2023-03-06 15:48:13 +0900

    수영
    경로변경

--- shcard_srt/js/shinsrt.css.js
+++ shcard_srt/js/shinsrt.css.js
@@ -1,11 +1,11 @@
 (function () {
     var vers = new Date().getDate() + '' + (new Date().getHours());
     // 캐시되어야 하는 공통 파일들 (외부 라이브러리로 변경이 없는 파일)
-    document.write('<li' + 'nk rel="stylesheet" href="' + '/css/libs/swiper-bundle.min.css" media="all" />');
-    document.write('<li' + 'nk rel="stylesheet" href="' + '/css/libs/jquery-ui.min.css" media="all" />');
-    document.write('<li' + 'nk rel="stylesheet" href="' + '/css/font.css" media="all" />');
-    document.write('<li' + 'nk rel="stylesheet" href="' + '/css/datepicker.css" media="all" />');
-    document.write('<li' + 'nk rel="stylesheet" href="' + '/css/common.css?ver=' + vers + '" media="all" />');
+    document.write('<li' + 'nk rel="stylesheet" href="' + '../../css/libs/swiper-bundle.min.css" media="all" />');
+    document.write('<li' + 'nk rel="stylesheet" href="' + '../../css/libs/jquery-ui.min.css" media="all" />');
+    document.write('<li' + 'nk rel="stylesheet" href="' + '../../css/font.css" media="all" />');
+    document.write('<li' + 'nk rel="stylesheet" href="' + '../../css/datepicker.css" media="all" />');
+    document.write('<li' + 'nk rel="stylesheet" href="' + '../../css/common.css?ver=' + vers + '" media="all" />');
     // document.write('<li' + 'nk rel="shortcut icon" type="image/x-icon" hre' + 'f="/pconts/images/ico_favicon96.ico?v=2020" />\n'
 	// + '<li' + 'nk rel="stylesheet" hre' + 'f="' + '/pconts/css/shcard/shc-fonts.css?ver=' + ver + '" media="all"  />\n'
 })();
